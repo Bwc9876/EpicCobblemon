86ac89090cee6ef0acdf707b842ad55da61d11ca
Ben C <bwc9876@gmail.com>
2025-12-15 17:56:39 -0500

    Add Cobblemon Unchained

--- flake.nix
+++ flake.nix
@@ -1,11 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flakelight.url = "github:nix-community/flakelight";
   };
   outputs = { flakelight, ... }@inputs:
     flakelight ./. {
       inherit inputs;
+			package = pkgs: pkgs.writeShellApplication {
+				name = "epic-cobblemon-serve";
+
+				runtimeInputs = [pkgs.packwiz];
+
+				text = ''
+					cd ${./.}
+					packwiz serve "$@"
+				'';
+			};
       devShell.packages = pkgs: with pkgs; [ packwiz ];
     };
 }
